df82816894bdc1e8603c9291c362e725deff24a3
digmar00 <marcodignazio.md2@gmail.com>
2022-06-15 14:51:03 +0200

    modificata la classe MainTest

--- GitProject/src/main/MainTest.java
+++ GitProject/src/main/MainTest.java
@@ -6,7 +6,9 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		Persona p = new Persona("Fabrizio", "Fonte");
+		Persona p2 = new Persona("Marco", "Dignazio");
 		System.out.println(p.toString());
+		System.out.println(p2.toString());
 
 	}
 
