8145e30f14a01a8c6005dc4fb389021d42695a5d
Fabrizio <gaetano.fabrizio.fonte-cic@ibm.com>
2022-06-15 14:38:22 +0200

    primo commit eclipse

---
+++ GitProject/src/main/MainTest.java
@@ -0,0 +1,13 @@
+package main;
+
+import classi.Persona;
+
+public class MainTest {
+
+	public static void main(String[] args) {
+		Persona p = new Persona("Fabrizio", "Fonte");
+		System.out.println(p.toString());
+
+	}
+
+}
