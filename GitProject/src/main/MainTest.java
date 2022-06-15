ec5facb5a15308b433455fc7bf0447e03031756f
Fabrizio <gaetano.fabrizio.fonte-cic@ibm.com>
2022-06-15 14:51:55 +0200

    modificato metodo main

--- GitProject/src/main/MainTest.java
+++ GitProject/src/main/MainTest.java
@@ -6,6 +6,7 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		Persona p = new Persona("Fabrizio", "Fonte");
+		Persona p2 = new Persona("Rocco", "Papaleo");
 		System.out.println(p.toString());
 
 	}
